f61f33bbb00bf182eb58e25572cf84f7b9e68591
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2022-10-15 11:39:14 +0300

    refactor index js (from commonjs to module)

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,9 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+import functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello World!');
 });
