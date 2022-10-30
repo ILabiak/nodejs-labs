9efc480ee26176917de2d33f7cc5d934108308a0
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2022-10-30 14:33:30 +0200

    delete useless code in functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,4 @@
 import functions from 'firebase-functions';
-import admin from 'firebase-admin';
-
-admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
